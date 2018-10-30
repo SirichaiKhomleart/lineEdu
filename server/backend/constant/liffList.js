72072792dd8543d6c4c04810305d28631d66b504
pasinJ <pasin@iapp.co.th>
2018-10-30 22:25:49 +0700

    update liff link

--- server/backend/constant/liffList.js
+++ server/backend/constant/liffList.js
@@ -1,4 +1,4 @@
 module.exports = Object.freeze({
-    addUser: "line://app/1609431105-MZPlnjJ4",
-    createClassroom: "line://app/1609431105-Q528JGvM"
+    addUser: "line://app/1609431105-5QbaY9JV",
+    createClassroom: "line://app/1609431105-VvrWKEJn"
 });
